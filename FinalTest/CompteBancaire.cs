0a13c1a90b87187d7d84ded52b26271736572bc4
Mohamed Boudrij <p1414174@IUTDOUAB2PC13.univ-lyon1.fr>
2015-06-25 11:30:49 +0200

    Realisation d un retrait

--- FinalTest/CompteBancaire.cs
+++ FinalTest/CompteBancaire.cs
@@ -5,10 +5,17 @@ namespace FinalTest
     public class CompteBancaire
     {
         private readonly CompteCréé _compteCréé;
+        private readonly DépotRéalisé _depoDépotRéalisé;
 
         public CompteBancaire(CompteCréé compteCréé)
         {
             _compteCréé = compteCréé;
+        }
+
+        public CompteBancaire(CompteCréé compteCréé,DépotRéalisé depoDépotRéalisé)
+        {
+            _compteCréé = compteCréé;
+            _depoDépotRéalisé = depoDépotRéalisé;
         }
 
         public static IEnumerable<IEvenementMetier> Ouvrir(string _numéroDeCompte, int autorisationDeCrédit)
@@ -19,6 +26,11 @@ namespace FinalTest
         public IEnumerable<IEvenementMetier>  FaireUnDepot(Montant montantDepot, System.DateTime dateDepot)
         {
             yield return new DépotRéalisé(this._compteCréé._numéroDeCompte, montantDepot, dateDepot);
+        }
+
+        public IEnumerable<IEvenementMetier> FaireUnRetrait(Montant montantRetrait, System.DateTime dateRetrait)
+        {
+            yield return new RetraitRéalisé(this._compteCréé._numéroDeCompte,montantRetrait, dateRetrait);
         }
     }
 }
---
+++ FinalTest/RetraitRéalisé.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest
+{
+    public struct RetraitRéalisé : IEvenementMetier
+    {
+        private string _numéroDeCompte;
+        private Montant montantRetrait;
+        private DateTime dateRetrait;
+
+        public RetraitRéalisé(string _numéroDeCompte, Montant montantRetrait, DateTime dateRetrait)
+        {
+            // TODO: Complete member initialization
+            this._numéroDeCompte = _numéroDeCompte;
+            this.montantRetrait = montantRetrait;
+            this.dateRetrait = dateRetrait;
+        }
+    }
+}
